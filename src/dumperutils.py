117d401f47364b5cb9f41519a3e0009c1b056478
nicolapcweek94 <citizenwasp@gmail.com>
2014-04-29 12:41:14 +0200

    Beautified some shit

--- src/dumperutils.py
+++ src/dumperutils.py
@@ -1,22 +1,27 @@
 import urllib.request, re
 
-def store(name, url):
-    print("[~] Downloading " + url + " -> " + name)
-    f = urllib.request.urlopen(url)
-    with open(name, "b+w") as g:
-        g.write(f.read())
+def store(url, name = ""):
+    if name == "":
+        r = re.search("([^/]+?)(.png|.jpg|.jpeg)$", url)
+        if r:
+            store(url, r.group(1) + r.group(2))
+    else:
+        print("[~] Downloading " + url + " -> " + name)
+        f = urllib.request.urlopen(url)
+        with open(name, "b+w") as g:
+            g.write(f.read())
 
 def imguralbum(url):
     html = urllib.request.urlopen(url).read().decode("UTF-8")
     for s in re.findall("<a.+?class=\"zoom\".+?href=\"(.+?)\">", html):
         r = re.search("([^/]+?)(.png|.jpg|.jpeg)$", s)
-        store(r.group(1) + r.group(2), "https:" + s)
+        store("https:" + s, r.group(1) + r.group(2))
 
 def imgursingle(url):
     html = urllib.request.urlopen(url).read().decode("UTF-8")
     r = re.search("<a.+?href=\".+?/([^/]+?)(.png|.jpg|.jpeg)\">", html)
-    store(r.group(1) + r.group(2), url)
+    store(url, r.group(1) + r.group(2))
 
 def steamscreen(url):
     r = re.search(".+?/([^/]+?)/$", url)
-    store(r.group(1) + ".jpg", url)
+    store(url, r.group(1) + ".jpg")
